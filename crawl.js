712d60c6a1f6e573d6625bfcaab109daea1e3cd4
James Mark <james.david.mark@gmail.com>
2024-01-10 16:33:54 -0600

    Moving back to main.js

--- crawl.js
+++ crawl.js
@@ -21,28 +21,19 @@ function getURLsFromHTML(htmlBody, baseURL) {
     return linkList
 }
 
+function crawlPage(currentURL) {
+    try {
+        const resp = fetch(currentURL)
+        if (resp.status >= 400 || resp.status < 500) {
+            throw new Error(`Got status code ${resp.status}`)
+        }
+    } catch (error) {
+
+    }
+}
+
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage,
 }
-
-function main() {
-    /*All your main function should do at this point is:
-    
-    If the number of CLI arguments is less than 1, print an error and exit.
-    If the number of CLI arguments is more than 1, print an error and exit.
-    If we have exactly one CLI argument, it's the "baseURL", so print some kind of message letting the user know the crawler is starting at that baseURL*/
-    if (argv.length < 3) {
-        console.log("Error: Not enough arguments. Please provide a url")
-        process.exit()
-    }
-    if (argv.length > 3) {
-        console.log("Error: Please only provide one argument")
-        process.exit()
-    }
-    const baseURL = argv[2]
-    console.log(`Parsing beginning at ${baseURL}`)
-}
-
-
-main()
---
+++ main.js
@@ -0,0 +1,22 @@
+const { crawlPage } = require('./crawl')
+
+function main() {
+    /*All your main function should do at this point is:
+    
+    If the number of CLI arguments is less than 1, print an error and exit.
+    If the number of CLI arguments is more than 1, print an error and exit.
+    If we have exactly one CLI argument, it's the "baseURL", so print some kind of message letting the user know the crawler is starting at that baseURL*/
+    if (argv.length < 3) {
+        console.log("Error: Not enough arguments. Please provide a url")
+        process.exit()
+    }
+    if (argv.length > 3) {
+        console.log("Error: Please only provide one argument")
+        process.exit()
+    }
+    const baseURL = argv[2]
+    console.log(`Parsing beginning at ${baseURL}`)
+}
+
+
+main()
