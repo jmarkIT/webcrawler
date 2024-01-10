cdd09094de3dce485b15634cd09cbff64056cd6e
James Mark <james.david.mark@gmail.com>
2024-01-10 16:14:18 -0600

    Creating Main function, finishing getURLsFromHTML

--- crawl.js
+++ crawl.js
@@ -1,3 +1,6 @@
+const { JSDOM } = require('jsdom')
+const { argv } = require('node:process')
+
 function normalizeURL(url) {
     const fullURL = new URL(url)
     const host = fullURL.host
@@ -5,6 +8,41 @@ function normalizeURL(url) {
     return `${host}${path}`
 }
 
-module.exports = {
-    normalizeURL
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urlLinks = new JSDOM(htmlBody).window.document.links
+    let linkList = []
+    for (let link of urlLinks)
+        if (link.href[0] !== '/') {
+            linkList.push(link.href)
+        } else {
+            const expandedURL = `${baseURL}${link.href}`
+            linkList.push(expandedURL)
+        }
+    return linkList
 }
+
+module.exports = {
+    normalizeURL,
+    getURLsFromHTML
+}
+
+function main() {
+    /*All your main function should do at this point is:
+    
+    If the number of CLI arguments is less than 1, print an error and exit.
+    If the number of CLI arguments is more than 1, print an error and exit.
+    If we have exactly one CLI argument, it's the "baseURL", so print some kind of message letting the user know the crawler is starting at that baseURL*/
+    if (argv.length < 3) {
+        console.log("Error: Not enough arguments. Please provide a url")
+        process.exit()
+    }
+    if (argv.length > 3) {
+        console.log("Error: Please only provide one argument")
+        process.exit()
+    }
+    const baseURL = argv[2]
+    console.log(`Parsing beginning at ${baseURL}`)
+}
+
+
+main()
--- crawl.test.js
+++ crawl.test.js
@@ -1,18 +1,47 @@
 const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 
-test('Test 1', () => {
+test('normalizeURL Test 1', () => {
     expect(normalizeURL("https://blog.boot.dev/path/")).toBe("blog.boot.dev/path")
 })
 
-test('Test 2', () => {
+test('normalizeURL Test 2', () => {
     expect(normalizeURL("http://blog.boot.dev/path/")).toBe("blog.boot.dev/path")
 })
 
-test('Test 3', () => {
+test('normalizeURL Test 3', () => {
     expect(normalizeURL("https://blog.boot.dev/path")).toBe("blog.boot.dev/path")
 })
 
-test('Test 4', () => {
+test('normalizeURL Test 4', () => {
     expect(normalizeURL("http://blog.boot.dev/path")).toBe("blog.boot.dev/path")
+})
+
+test('getURLsFromHTML 1 link', () => {
+    const testHTML = `<html>
+    <body>
+        <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+    </body>
+    </html>`
+    expect(getURLsFromHTML(testHTML, 'https://boot.dev')).toEqual(['https://blog.boot.dev/'])
+})
+
+test('getURLsFromHTML 2 links', () => {
+    const testHTML = `<html>
+    <body>
+        <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+        <p>Here is some junk</p>
+        <a href="https://boot.dev/catastrophe"><span>Go to Boot.dev</span></a>
+    </body>
+    </html>`
+    expect(getURLsFromHTML(testHTML, 'https://boot.dev')).toEqual(['https://blog.boot.dev/', 'https://boot.dev/catastrophe'])
+})
+
+test('getURLsFromHTML relative url', () => {
+    const testHTML = `<html>
+    <body>
+        <a href="/catastrophe"><span>Go to Boot.dev</span></a>
+    </body>
+    </html>`
+    expect(getURLsFromHTML(testHTML, 'https://boot.dev')).toEqual(['https://boot.dev/catastrophe'])
 })
